3fd1e7299acbf7ccceba27c3346d6f7b5390b24c
Ben Jubb <bjubb@vividsolutions.com>
2026-02-26 16:03:53 -0800

    ability to add custom columns to list-select

--- packages/angular-components/projects/nr-ngx-component-lib/src/components/list-select/list-select.component.ts
+++ packages/angular-components/projects/nr-ngx-component-lib/src/components/list-select/list-select.component.ts
@@ -1,7 +1,8 @@
-import { booleanAttribute, ChangeDetectionStrategy, Component, EventEmitter, Input, numberAttribute, OnChanges, Output, SimpleChanges } from "@angular/core";
+import { AfterContentInit, booleanAttribute, ChangeDetectionStrategy, Component, ContentChildren, EventEmitter, Input, numberAttribute, OnChanges, Output, QueryList, SimpleChanges, ViewChild } from "@angular/core";
 import { Observable, of } from "rxjs";
 import { LoadRowListResult, RowListBase, RowListState } from "../../directives/row-list.base";
 import { CodeDescription } from "../../utils/code-table.util";
+import { MatColumnDef, MatTable } from "@angular/material/table";
 
 @Component({
     selector: "nrcl-list-select",
@@ -9,25 +10,44 @@ import { CodeDescription } from "../../utils/code-table.util";
     styleUrl: "./list-select.component.scss",
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ListSelectComponent extends RowListBase<{},CodeDescription> implements OnChanges {
+export class ListSelectComponent<T> extends RowListBase<{},CodeDescription> implements OnChanges, AfterContentInit {
     @Input() options: CodeDescription[]
     @Input() value: string[]
     @Input() descriptionLabel = 'Description'
     @Input( { transform: booleanAttribute } ) single 
     @Input() noRowsMessage = "No items have been added."
+    @Input() displayColumnsProvider: ( cols: string[] ) => string[] = ( cols ) => cols
+    @Input() filterProvider: ( option: CodeDescription ) => boolean = () => true
    
     @Output() valueChange = new EventEmitter<string[]>();
+    @Output() filterClear = new EventEmitter<void>();
+    
+    @ViewChild( MatTable, {static: true} ) table!: MatTable<T>;
+    
+    @ContentChildren( MatColumnDef ) columnDefs!: QueryList<MatColumnDef>;
     
     searchText
     searchRegexp: RegExp
-    displayColumns = [ 'description', 'addRemove' ]
+    defaultDisplayColumns = [ 'description', 'addRemove' ]
+    displayColumns = []
 
     ngOnChanges( changes: SimpleChanges ): void {
-        console.log(changes)
+        // console.log(changes)
 
         if ( changes.options ) {
             this.refreshRowList()
         }
+    }
+
+    ngAfterContentInit(): void {
+        // console.log('ngAfterContentInit')
+        this.columnDefs.forEach(columnDef => this.table.addColumnDef(columnDef));
+
+        setTimeout( () => {
+            this.displayColumns = this.displayColumnsProvider( [ ...this.defaultDisplayColumns ] )
+            // console.log( this.displayColumns )
+            // this.changeDetectorRef.detectChanges()            
+        } )
     }
 
     get initialPageState(): RowListState<{}> {
@@ -45,7 +65,6 @@ export class ListSelectComponent extends RowListBase<{},CodeDescription> impleme
     }
     
     displayRowListPage( res: CodeDescription[] ): LoadRowListResult<CodeDescription> {
-        console.log(res)
         return {
             totalRowCount: res.length,
             rows: res
@@ -82,6 +101,7 @@ export class ListSelectComponent extends RowListBase<{},CodeDescription> impleme
     onClearFilters() {
         this.searchText = null
         this.searchRegexp = null
+        this.filterClear.emit()
         this.refreshRowList()
     }
 
@@ -101,14 +121,14 @@ export class ListSelectComponent extends RowListBase<{},CodeDescription> impleme
     }
 
     filterOption( option: CodeDescription ): boolean {
-        if ( this.searchRegexp?.test( option.description ) ) return true
+        if ( this.searchRegexp && !this.searchRegexp.test( option.description ) ) return false
 
-        return this._optionsFilter( option )
+        return this.filterProvider( option )
     }
 
-    private _optionsFilter: ( option: CodeDescription ) => boolean = () => true
+    // private _optionsFilter: ( option: CodeDescription ) => boolean = () => true
 
-    setOptionsFilter( filter: ( option: CodeDescription ) => boolean ) {
-        this._optionsFilter = filter
-    }
+    // setOptionsFilter( filter: ( option: CodeDescription ) => boolean ) {
+    //     this._optionsFilter = filter
+    // }
 }
