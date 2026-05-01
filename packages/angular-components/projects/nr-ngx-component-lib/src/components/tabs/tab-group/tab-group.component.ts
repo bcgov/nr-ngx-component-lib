7d94ffdd1bf003ab3a77261a6af5cb4b7e92430b
Ben Jubb <bjubb@vividsolutions.com>
2026-04-30 18:12:06 -0700

    wip

--- packages/angular-components/projects/nr-ngx-component-lib/src/components/tabs/tab-group/tab-group.component.ts
+++ packages/angular-components/projects/nr-ngx-component-lib/src/components/tabs/tab-group/tab-group.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, booleanAttribute, ChangeDetectorRef, Component, ContentChildren, ElementRef, inject, Input, QueryList } from '@angular/core';
+import { AfterContentInit, booleanAttribute, ChangeDetectorRef, Component, ContentChildren, ElementRef, inject, Input, OnChanges, QueryList, SimpleChanges } from '@angular/core';
 import { NrclBase } from '../../../directives/nrcl.base';
 import { TabComponent } from '../tab/tab.component';
 
@@ -6,16 +6,34 @@ import { TabComponent } from '../tab/tab.component';
     selector: 'nrcl-tab-group',
     templateUrl: './tab-group.component.html',
     styleUrl: './tab-group.component.scss',
+    host: {
+        '[class.look-standard]': 'isStandard',
+        '[class.look-classic]': 'isClassic',
+    }
 } )
-export class TabGroupComponent extends NrclBase {
-    // @Input() tooltip
-    // @Input() content
-    
+export class TabGroupComponent extends NrclBase implements OnChanges {
     elementRef = inject( ElementRef )
     changeDetectorRef = inject( ChangeDetectorRef )
+
+    // @Input() tooltip
+    // @Input() content
+    @Input( { transform: booleanAttribute } ) standard = false
+    @Input( { transform: booleanAttribute } ) classic = false
+    
+    isStandard
+    isClassic
 
     // tooltipContent
 
     @ContentChildren(TabComponent) tabs!: QueryList<TabComponent>;
+
+    ngOnChanges( changes: SimpleChanges ): void {
+        this.updateState()
+    }
+
+    updateState() {
+        this.isStandard = this.standard && !this.classic
+        this.isClassic = this.classic && !this.standard
+    }
 
 }
