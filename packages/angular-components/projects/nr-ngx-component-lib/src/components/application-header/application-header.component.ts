8bbda5ae28f097fe621b9b220b52cf8c213e41a4
Cody Bergin <cbergin@vividsolutions.com>
2026-09-09 12:55:15 -0700

    WFWX-3043 - Increase mobile support, update application header with menu item, tweak css for menu item in header

--- packages/angular-components/projects/nr-ngx-component-lib/src/components/application-header/application-header.component.ts
+++ packages/angular-components/projects/nr-ngx-component-lib/src/components/application-header/application-header.component.ts
@@ -5,6 +5,8 @@ import {
 } from "@angular/core";
 import { NrclBase } from '../../directives/nrcl.base';
 
+import { MenuItem, MenuTrigger } from '../menu/menu.component';
+
 @Component({
     selector: "nrcl-application-header",
     templateUrl: "./application-header.component.html",
@@ -33,5 +35,12 @@ export class ApplicationHeaderComponent extends NrclBase{
 
     @Input() menuTitle = "";
 
+    @Input() menuItems: MenuItem[] = [];
+
+    @Input()
+    menuTrigger: MenuTrigger = {
+        label: 'Menu',
+        icon: 'menu'
+    };
     
 }
