710f5b0e7fc364b1c0b8d11966307a3d0b35b21f
Ben Jubb <bjubb@vividsolutions.com>
2026-09-16 08:11:35 -0700

    revert some button changes

--- packages/angular-components/projects/nr-ngx-component-lib/src/components/button/button.component.ts
+++ packages/angular-components/projects/nr-ngx-component-lib/src/components/button/button.component.ts
@@ -24,9 +24,6 @@ import { ConfigurationSubscriberBase } from '../../directives/configuration-subs
 export class ButtonComponent extends ConfigurationSubscriberBase implements OnChanges {
     zone = inject( NgZone )
 
-    @ViewChild('focusTarget', { read: ElementRef })
-    focusTarget?: ElementRef<HTMLElement>;
-
     @Input() label?: string
     @Input() ariaLabel?: string;
     @Input() icon?: string
@@ -142,9 +139,5 @@ export class ButtonComponent extends ConfigurationSubscriberBase implements OnCh
         }
 
         this.useContent = !this.hasLabel && !this.hasIconLeft && !this.hasIconRight && !this.hasIconCompact
-    }
-
-    focus(): void {
-        this.focusTarget?.nativeElement.focus();
     }
 }
